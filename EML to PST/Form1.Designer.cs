e2bac538bc1f6ccc0c7586cdb4e584e7b62a11be
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-11-08 13:49:08 +0100

    Ad installer

--- EML to PST/Form1.Designer.cs
+++ EML to PST/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -141,6 +142,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Konverter EML u PST";
             this.Load += new System.EventHandler(this.Form1_Load);
